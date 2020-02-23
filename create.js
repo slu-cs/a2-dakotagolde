058b456f6b4b60f78159fb943c1e819a89ed9c97
Dakota Golde <dakotagolde@Dakotas-MacBook-Pro-2.local>
2020-02-23 18:31:08 -0500

    HW 2

--- create.js
+++ create.js
@@ -1,44 +1,39 @@
-// Store some data in the voter database
 
+
+const fs = require('fs');
+const readline = require('readline');
 const mongoose = require('mongoose');
 const connect = require('./db');
 const voter = require('./schema');
-const fs = require('fs');
-const readline = require('readline');
 
-connect();  // To the database
-
-// Read majors.csv (which needs to be on your VM when you run this code).
+connect(); // To the database
 
 const file = readline.createInterface({
   input: fs.createReadStream('voters.csv')
+
 });
 
-// Create an array of objects, so that each line of the file is represented by an object with three properties.
 const rows = [];
 file.on('line', function(line) {
-  const column = line.split(',');
-  rows.push(
-    new voter({
-      first: column[0],
-      last: column[1],
-      zip: column[2],
-      history: column[3]
-    }).save()
+  const columns = line.split(',');
+  votes.push(
+        new voter({
+          first: columns[0],
+          last: columns[1],
+          zip: columns[2],
+          history: columns[3]
+    })
   );
 });
 
-// Let the popularity score for a discipline be the number of majors it has plus half the number of minors.
-// Sort disciplines by (increasing) popularity.
+
+
 file.on('close', function() {
-  mongoose.connection.dropDatabase()
-    .then(() => Promise.all(rows))
+mongoose.connection.dropDatabase()
+    .then(() => Promise.all(rows.map(d => d.save())))
     .then(() => mongoose.connection.close())
     .then(() => console.log('Database is ready.'))
-    .catch(error => console.error(error.stack))
-});
+    .catch(error => console.log(error.stack));
 
-
-
-// Condensed callbacks in a sequence (no nesting)
-// Reset the data
+  });
+\
