e322a337527b173bb9e3f9e455b4541bda99c862
Dakota Golde <dakotagolde@Dakotas-MacBook-Pro-2.local>
2020-02-23 18:58:46 -0500

    HW 2

--- query.js
+++ query.js
@@ -12,7 +12,7 @@ query.exec(function(error, professors) {
 });*/
 
 const queries = [
-  
+
   // How many registered voters live in the Canton zip code (13617)?
   Voter.find().where('zip').equals('13617'),
 
@@ -35,7 +35,7 @@ Promise.all(queries)
     console.log('Number of registered voters in Canton: ', results[0].length);
     console.log('Voters with name the first name STARR: ', results[1].map(v => (v.first + " " + v.last)));
     console.log('Number of 2016 general election Voters: ', results[2].length);
-    console.log('last-name that comes last in the county in alphabetical order:', results[3].map(v => v.last));
+    console.log('The last-name that comes last in the county in alphabetical order:', results[3].map(v => v.last));
     console.log('Number of Distinct zip codes: ', results[4].length);
     mongoose.connection.close();
   }).catch(error => console.error(error.stack));
