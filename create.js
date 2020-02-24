dc92de6c3eed584b8b5be53b7593c2c3fa689326
Dakota Golde <dakotagolde@Dakotas-MacBook-Pro-2.local>
2020-02-23 19:15:47 -0500

    HW 2

--- create.js
+++ create.js
@@ -2,34 +2,33 @@ const mongoose = require('mongoose');
 const connect = require('./db');
 const Voter = require('./schema');
 const fs = require('fs');
-const readline= require('readline');
-
-connect(); // To the database
-
-const file = readline.createInterface ({
+const readline = require('readline');
+const file = readline.createInterface({
   input: fs.createReadStream('voters.csv')
 });
 
-// create an array of objects, so that each line of the file is represented by an object with 4 properties
-const voterRows = [];
-file.on('line', function(line)  {
-  const columns = line.split(',');
-  voterRows.push(new Voter({
-    firstName: columns[0],
-    lastName: columns[1],
-    zipCode: columns[2],
-    historyString: columns[3]
-  })
-);
+connect();
+
+// Empty array of voter data
+voter_data = []
+
+// Read the csv and put information into array
+file.on('line', function(line) {
+    const columns = line.split(',');
+    voter_data.push(new Voter({
+        first_name: columns[0],
+        last_name: columns[1],
+        zipcode: columns[2],
+        history: columns[3]
+        })
+    );
 });
 
-
-file.on('close', function()  {
-// Reset the data
-mongoose.connection.dropDatabase()
-    .then(() => Promise.all(voterRows.map(d => d.save())))
-    // close connection
-    .then(() => mongoose.connection.close())
-    .then(() => console.log('Database is ready.'))
-    .catch(error => console.error(error.stack));
+// Close the file and save the voters to the database
+file.on('close', function() {
+    mongoose.connection.dropDatabase()
+        .then(() => Promise.all(voter_data.map(voter => voter.save())))
+        .then(() => mongoose.connection.close())
+        .then(() => console.log('Database is ready.'))
+        .catch(error => console.error(error.stack));
 });
--- schema.js
+++ schema.js
@@ -1,22 +1,18 @@
-// Define a plan for a collection
-
 const mongoose = require('mongoose');
-
 
 // Schema for a collection of voters
 const Voter = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  zipCode: String,
-  historyString: String
+  first_name: String,
+  last_name: String,
+  zipcode: Number,
+  history: String
 });
 
-
 // Speed up queries on all fields
-Voter.index({firstName: 1});   // I want to be able to look up professors by their name and quicly
-Voter.index({lastName: 1});
-Voter.index({zipCode: 1});
-Voter.index({historyString: 1});
+Voter.index({first_name: 1});
+Voter.index({last_name: 1});
+Voter.index({zipcode: 1});
+Voter.index({history: 1});
 
-// Compile and export this Schema
+// Compile and export this schema
 module.exports = mongoose.model('Voter', Voter);
