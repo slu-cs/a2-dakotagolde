41ceea1bcfb43e56d01087a69508c6240d0ae0af
Dakota Golde <dakotagolde@Dakotas-MacBook-Pro-2.local>
2020-02-23 19:12:58 -0500

    HW 2

--- create.js
+++ create.js
@@ -1,36 +1,35 @@
-const fs = require('fs');
-const readline = require('readline');
 const mongoose = require('mongoose');
 const connect = require('./db');
 const Voter = require('./schema');
+const fs = require('fs');
+const readline= require('readline');
 
 connect(); // To the database
 
-const file = readline.createInterface({
+const file = readline.createInterface ({
   input: fs.createReadStream('voters.csv')
-
 });
 
-const votes = [];
-file.on('line', function(line) {
+// create an array of objects, so that each line of the file is represented by an object with 4 properties
+const voterRows = [];
+file.on('line', function(line)  {
   const columns = line.split(',');
-  votes.push(
-        new Voter({
-          first: columns[0],
-          last: columns[1],
-          zip: columns[2],
-          history: columns[3]
-    })
-  );
+  voterRows.push(new Voter({
+    firstName: columns[0],
+    lastName: columns[1],
+    zipCode: columns[2],
+    historyString: columns[3]
+  })
+);
 });
 
 
-
-file.on('close', function() {
+file.on('close', function()  {
+// Reset the data
 mongoose.connection.dropDatabase()
-    .then(() => Promise.all(votes.map(d => d.save())))
+    .then(() => Promise.all(voterRows.map(d => d.save())))
+    // close connection
     .then(() => mongoose.connection.close())
     .then(() => console.log('Database is ready.'))
-    .catch(error => console.log(error.stack));
-
-  });
+    .catch(error => console.error(error.stack));
+});
--- schema.js
+++ schema.js
@@ -2,19 +2,21 @@
 
 const mongoose = require('mongoose');
 
+
 // Schema for a collection of voters
 const Voter = new mongoose.Schema({
-    first: String,
-    last: String,
-    zip: String,
-    history: String
-    });
+  firstName: String,
+  lastName: String,
+  zipCode: String,
+  historyString: String
+});
+
 
 // Speed up queries on all fields
-Voter.index({frist: 1});
-Voter.index({last: 1});
-Voter.index({zip: 1});
-Voter.index({history: 1});
+Voter.index({firstName: 1});   // I want to be able to look up professors by their name and quicly
+Voter.index({lastName: 1});
+Voter.index({zipCode: 1});
+Voter.index({historyString: 1});
 
-// Compile and export this schema
+// Compile and export this Schema
 module.exports = mongoose.model('Voter', Voter);
