028da87454523e6f01ad38bfcf9824a5e0d72e72
Dakota Golde <dakotagolde@Dakotas-MacBook-Pro-2.local>
2020-02-23 19:39:31 -0500

    HW 2

--- query.js
+++ query.js
@@ -14,13 +14,13 @@ query.exec(function(error, professors) {
 const queries = [
 
   // How many registered voters live in the Canton zip code (13617)?
-  Voter.find().where('zip').equals('13617').count(),
+  Voter.find().where('zip').equals('13617').countDocuments(),
 
   // What are the full names of all the registered voters whose first-name is STARR?
   Voter.find().where('first').equals('STARR'),
 
   // How many people voted in the 2016 general election (GE16)?
-  Voter.find({ "history": /GE16/i }).count(),
+  Voter.find({ "history": /GE16/i }).countDocuments(),
 
   // What is the last-name that comes last in the county in alphabetical order?
   Voter.find().sort('-last').limit(1),
